d722835454c49a2f7bb4c4902ce5f76eca641e8d
huangxichu <330679601@qq.com>
2018-05-28 16:13:58 +0800

    第二次

--- homeIcon/models.py
+++ homeIcon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class HomeIcon(models.Model):
+    id = models.CharField(primary_key=True,max_length=32)
+    c_index = models.IntegerField(max_length=3)
+    status = models.CharField(max_length=1)
+    icon = models.DateTimeField(max_length=256)
+    class Meta:
+        db_table = 'tb_home_icon'  # 自定义表名称
